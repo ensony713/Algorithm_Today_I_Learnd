cc076531c721bdc385dc1cdb7782d8f257abeae4
seon <ensony713@gmail.com>
2022-03-15 13:47:50 +0900

    vector is slow...

--- 2164.cpp
+++ 2164.cpp
@@ -1,22 +1,76 @@
 #include <iostream>
-#include <vector>
+#define MAX 500005
 using namespace std;
 
+class Queue {
+private:
+	int num[MAX] = { 0 };
+	int start, q_end, tmp;
+	int set(int index) {
+		index = index % MAX;
+		if (index < 0)
+			index += MAX;
+		return index;
+	}
+
+public:
+	int empty() {
+		if (start == q_end)
+			return 1;
+		else
+			return 0;
+	}
+
+	void push(int a) {
+		num[q_end] = a;
+		q_end = set(q_end + 1);
+		return;
+	}
+
+	int pop() {
+		if (empty())
+			return -1;
+
+		tmp = num[start];
+		start = set(start + 1);
+		return tmp;
+	}
+
+	int size() {
+		if (q_end >= start)
+			return q_end - start;
+		else
+			return MAX + q_end - start;
+	}
+
+	int front() {
+		if (empty())
+			return -1;
+		return num[start];
+	}
+
+	int back() {
+		if (empty())
+			return -1;
+		return  num[q_end - 1];
+	}
+};
+
 int N, tmp;
-vector<int> que;
+Queue que;
 
 int main() {
 	cin >> N;
 
-	for (int i = N; i > 0; i--)
-		que.push_back(i);
+	for (int i = 1; i <= N; i++)
+		que.push(i);
 
 	while (que.size() > 1) {
-		que.pop_back();
-		tmp = que[que.size() - 1];
-		que.pop_back();
-		que.insert(que.begin(), tmp);
+		que.pop();
+		tmp = que.front();
+		que.pop();
+		que.push(tmp);
 	}
-	cout << que[0] << "\n";
+	cout << que.front() << "\n";
 	return 0;
 }
