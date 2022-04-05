677f14bbc90c6d0f26bcc80bf6346a78da0f66a3
seon <ensony713@gmail.com>
2022-04-05 15:33:00 +0900

    first

---
+++ 1692.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long long A, B, C;
+
+long long pow(long long b) {
+	if (b == 0) return 1;
+	
+	long long tmp = pow(b / 2) % C;
+	if ((b % 2) == 0)
+		return ((tmp % C) * (tmp % C)) % C;
+	else
+		return ((tmp % C) * (tmp % C) * (A % C)) % C;
+}
+
+int main() {
+	cin >> A >> B >> C;
+	cout << pow(B) << "\n";
+	return 0;
+}
