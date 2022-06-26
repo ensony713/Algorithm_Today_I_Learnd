e41caec7318e25bc6dae1b75c5d695244da7daa1
seon <80087294+ensony713@users.noreply.github.com>
2022-06-26 21:15:56 +0900

    Update 10809.cpp

--- String/10809.cpp
+++ String/10809.cpp
@@ -1,22 +1,22 @@
-//#include <iostream>
-//#include <string>
-//#include <memory.h>
-//using namespace std;
-//
-//string input;
-//int alphabet[26] = { 0 }, index_ = 0;
-//
-//int main() {
-//	memset(alphabet, -1, sizeof(alphabet));
-//
-//	cin >> input;
-//	for (int i = 0; i < input.size(); i++) {
-//		index_ = (int)input[i] - 97;
-//		if (alphabet[index_] == -1)
-//			alphabet[index_] = i;
-//	}
-//	for (int i = 0; i < 26; i++)
-//		cout << alphabet[i] << " ";
-//	cout << "\n";
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+#include <memory.h>
+using namespace std;
+
+string input;
+int alphabet[26] = { 0 }, index_ = 0;
+
+int main() {
+	memset(alphabet, -1, sizeof(alphabet));
+
+	cin >> input;
+	for (int i = 0; i < input.size(); i++) {
+		index_ = (int)input[i] - 97;
+		if (alphabet[index_] == -1)
+			alphabet[index_] = i;
+	}
+	for (int i = 0; i < 26; i++)
+		cout << alphabet[i] << " ";
+	cout << "\n";
+	return 0;
+}
