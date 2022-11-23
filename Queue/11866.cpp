8b50181c7945ac2e6c69bf4caa391abd6ca6107a
seonu <ensony713@gmail.com>
2022-11-23 17:23:24 +0900

    11/23 Queue

--- Queue/11866.cpp
+++ Queue/11866.cpp
@@ -3,100 +3,77 @@
 using namespace std;
 
 class Queue {
-private:
-	int start, end_, tmp;
-	int value[MAX] = { 0 };
-	int set(int index) {
-		index = index % MAX;
-		if (index < 0)
-			index += MAX;
-		return index;
-	}
-public:
-	Queue() {
-		this->start = 0;
-		this->end_ = 0;
-		this->tmp = 0;
+	int front, end;
+	int data[MAX] = { 0 };
+	int set(int i) {
+		i %= MAX;
+		if (i < 0) i += MAX;
+		return i;
 	}
 
-	int empty() {
-		if (this->start == this->end_)
-			return 1;
-		else
-			return 0;
+public:
+	Queue() {
+		front = 0;
+		end = 0;
 	}
 
 	void push(int x) {
-		this->value[this->end_] = x;
-		this->end_ = this->set(this->end_ + 1);
-		return;
+		data[end] = x;
+		end = set(end + 1);
 	}
 
 	int pop() {
-		if (this->empty())
-			return -1;
+		if (front == end) return -1;
 
-		this->tmp = value[this->start];
-		this->start = this->set(this->start + 1);
-		return this->tmp;
+		int tmp = data[front];
+		front = set(front + 1);
+		return tmp;
 	}
 
 	int size() {
-		if (this->end_ >= this->start)
-			return this->end_ - this->start;
-		else
-			return MAX + this->end_ - this->start;
+		if (front > end) return MAX + (end - front);
+		else return end - front;
 	}
 
-	int front() {
-		if (this->empty())
-			return -1;
-		return this->value[this->start];
-	}
-
-	int back() {
-		if (this->empty())
-			return -1;
-		return this->value[this->end_ - 1];
-	}
-
-	void show() {
-		for (int i = this->start; i < this->end_; i++)
-			cout << this->value[i] << " ";
-		cout << "\n";
+	void print() {
+		for (int i = 0; i < this->size(); i++) {
+			cout << data[set(front + i)] << " ";
+		}
+		cout << '\n';
 	}
 };
 
-int N, K, counter = 1, index = 0, tmp;
-int answer[MAX];
 Queue q;
 
-int main() {
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++)
-		q.push(i);
+int main(int argc, char** argv) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	while (q.size() > 1) {
-		if (counter == K) {
-			counter = 1;
-			answer[index++] = q.pop();
+	int n, k, count = 0;
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	
+	cout << "<";
+
+	while (q.size() > 0) {
+
+		count++;
+
+		if (count == k) {
+			cout << q.pop();
+			count = 0;
+
+			if (q.size() != 0) cout << ", ";
+			else cout << ">";
 		}
 		else {
-			counter++;
-			tmp = q.pop();
-			q.push(tmp);
+			q.push(q.pop());
 		}
 	}
-
-	for (; !q.empty(); index++)
-		answer[index] = q.pop();
-
-	cout << "<";
-	for (int i = 0; i < N; i++) {
-		if (i != N - 1)
-			cout << answer[i] << ", ";
-		else
-			cout << answer[i] << ">";
-	}
+	
 	return 0;
 }
